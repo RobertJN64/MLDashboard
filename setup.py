ca9af5a74fd82b4a366cded626ead1b79e374935
RobertJN <robertjnies@gmail.com>
2021-09-07 11:51:23 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='PythonExtended',
-    url='https://github.com/RobertJN64/PythonExtended',
+    name='MLDashboard',
+    url='https://github.com/RobertJN64/MLDashboard',
     author='Robert Nies',
     author_email='robertjnies@gamil.com',
     # Needed to actually package something
-    packages=['PythonExtended'],
-    # Needed for dependencies - but IDK how to use it properly
-    #extras_require=['matplotlib','pygame', 'RPi.GPIO'],
+    packages=['MLDashboard'],
     # *strongly* suggested for sharing
-    version='1.0.2',
+    version='0.0.1',
     # The license can be anything you like
     license='MIT',
-    description='A collection of libraries that run on top of existing python libraries. Used to make function easier. Focus on object oriented and functional programming',
+    description='Machine learning dashboard that integrates with tensorflow.',
     long_description=open('README.txt').read(),
 )
